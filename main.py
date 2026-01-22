8650ec39dfa8e2f7051a47ec398b9589e6a57577
Aryan <aryanjain9818@gmail.com>
2026-01-21 21:30:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -471,6 +471,17 @@ st.set_page_config(
     },
 )
 
+#google analytics setup
+st.html('<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-YT907KK9QV"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag("js", new Date());
+
+  gtag("config", "G-YT907KK9QV");
+</script>')
+
 
 #Title
 st.markdown(
@@ -537,5 +548,6 @@ with usage:
         #Warning if 
         st.warning('Please Upload A csv/xlsx File Under 200 Mb')
         st.stop()
+
 
 
